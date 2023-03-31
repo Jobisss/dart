b3e155f6137218421a7719d029939e3c590a32c0
JOAO GABRIEL DO PRADO SCHIANTI <joao.1941@aluno.pr.senac.br>
2023-03-31 12:07:24 -0300

    o exercício é o média

---
+++ rpg.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+void main(){
+  var random = Random();
+  print("Escolha uma ação (1) Bater (2) Defender");
+  print()
+
+
+  /*for(int x = 0 ; x < 100 ; x++){
+     var numrandom = random.nextInt(3);
+     print(numrandom);
+  }*/
+  
+
+}
---
+++ teste.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  minhafunc();
+}
+
+void minhafunc(){
+  print("Qual a sua idade?");
+  String? input = stdin.readLineSync();
+  print("Sua idade é $input.");
+}
